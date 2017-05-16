e85b54f53fa144b533e3794510adb8fce040367e
Carlos Parada Alfaya <carlos.p.a123@gmail.com>
2017-05-16 19:42:00 +0200

    Fixes in css
    Fixed on css the sticky nav and footer, and completed the style of the
    center image div
    
    //TODO:
    - Add the div for about and the div for career
    - Add the .js that adds the github projects

--- library/behaviour.js
+++ library/behaviour.js
@@ -1,4 +1,3 @@
-/*
 $(document).ready(function() {
     var container = $(this).find(".container");
     container.append("<div class ='projects'></div>");
@@ -8,8 +7,13 @@ $(document).ready(function() {
     // Request the list of repos from my github
     $.get("https://api.github.com/users/cpathb/repos", function(repos) {
         repos.forEach(function(repo) {
-            Sprojects.append("<div class = 'repo'><div class='" + repo.language + "'></div><div class='repoData'><span class='RepoName'>" + repo.name + "</span><span class='repoDesc'>" + repo.description + "</span></div></div>");
+            //projects.append("<div class='repo'><div class='repoLanguage'><span>Pruebas</span></div><div class='repoData'><div class='repoName'><span class='RepoName'>" + repo.name + "</span></div><br><div class='repoDesc'><span class='RepoDesc'>" + repo.description + "</span></div></div></div>");
+            //$(this).find(".repoLanguage").css("background-color:", "rgb(255, 0, 0)");
+            if (repo.description != null) {
+                projects.append("<table class='repo' cellspacing='0' cellpadding='0'><tr><td rowspan='2' class='repoLang'></td><td class='repoName'><a href='https://github.com/cpathb/" + repo.name + "'>" + repo.name + "</a></td></tr><tr><td class='repoDesc'>" + repo.description + "</td></tr></table>");
+            } else {
+                projects.append("<table class='repo' cellspacing='0' cellpadding='0'><tr><td rowspan='2' class='repoLang'></td><td class='repoName'><a href='https://github.com/cpathb/" + repo.name + "'>" + repo.name + "</a></td></tr><tr><td class='repoDesc'></td></tr></table>");
+            }
         }, this);
     });
 });
-*/
